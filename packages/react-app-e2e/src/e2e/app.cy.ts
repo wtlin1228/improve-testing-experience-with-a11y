8c4affa1ba38f6174b3f6268ee24c89592bb5d07
wtlin1228 <wtlin1228@gmail.com>
2023-06-12 22:35:05 +0800

    add demo for testing the four primary types of elements

--- packages/react-app-e2e/src/e2e/app.cy.ts
+++ packages/react-app-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,64 @@
-import { getGreeting } from '../support/app.po';
-
 describe('react-app', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('form', () => {
+    // get input by label
+    cy.get('[data-test-id="i18n.form.field.title"]').then(($label) => {
+      const forInput = $label.attr('for');
+      console.log(forInput);
+      cy.get(`[name="${forInput}"]`).type('test');
+    });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome react-app');
+    // get error message by input
+    cy.get('[name="title"]').then(($input) => {
+      const id = $input.attr('aria-describedby');
+      cy.get(`#${id}`).contains('Required');
+    });
+  });
+
+  it('graphical element', () => {
+    // get visual indication by aria-label
+    cy.get('[aria-label="hide"]').click();
+  });
+
+  it('description list', () => {
+    // get description by term
+    cy.get('[data-test-id="i18n.summary.price"]')
+      .closest('dl')
+      .find('dd')
+      .contains('$200');
+  });
+
+  it('table', () => {
+    // get table by caption
+    cy.get('[data-test-id="i18n.table.caption"]').closest('table');
+
+    // get table's header row
+    cy.get('[data-test-id="i18n.table.caption"]')
+      .closest('table')
+      .find('thead tr')
+      .within(() => {
+        cy.get('th').eq(0).contains('i18n.table.col.header.order_id');
+        cy.get('th').eq(1).contains('i18n.table.col.header.order_by');
+      });
+
+    // get table's body rows
+    cy.get('[data-test-id="i18n.table.caption"]')
+      .closest('table')
+      .find('tbody tr')
+      .first()
+      .within(() => {
+        cy.get('td').eq(0).contains('9mq1123p');
+        cy.get('td').eq(1).contains('Leo');
+      });
+
+    cy.get('[data-test-id="i18n.table.caption"]')
+      .closest('table')
+      .find('tbody tr')
+      .eq(1)
+      .within(() => {
+        cy.get('td').eq(0).contains('mkq12ie4');
+        cy.get('td').eq(1).contains('Leo');
+      });
   });
 });
--- packages/react-app/src/app/app.tsx
+++ packages/react-app/src/app/app.tsx
@@ -1,12 +1,71 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
-    <div>
-      <NxWelcome title="react-app" />
+    <div style={{ display: 'flex', gap: '10px', flexDirection: 'column' }}>
+      {/* form */}
+      <form>
+        <div>
+          <label htmlFor="title" data-test-id="i18n.form.field.title">
+            i18n.form.field.title
+          </label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            aria-describedby="title-error-message"
+          />
+        </div>
+        <div id="title-error-message">Required</div>
+      </form>
+
+      {/* graphic element */}
+      <button
+        aria-label="hide"
+        onClick={() => {
+          console.log('hide');
+        }}
+      >
+        🫣
+      </button>
+
+      {/* description list */}
+      <dl>
+        <div>
+          <div>
+            <dt data-test-id="i18n.summary.price">i18n.summary.price</dt>
+          </div>
+          <dd>$200</dd>
+        </div>
+      </dl>
+
+      {/* table */}
+      <table className={styles.table}>
+        <caption className={styles.caption} data-test-id="i18n.table.caption">
+          i18n.table.caption
+        </caption>
+        <thead className={styles.thead}>
+          <tr>
+            <th scope="col" data-test-id="i18n.table.col.header.order_id">
+              i18n.table.col.header.order_id
+            </th>
+            <th scope="col" data-test-id="i18n.table.col.header.order_by">
+              i18n.table.col.header.order_by
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={styles.td}>9mq1123p</td>
+            <td className={styles.td}>Leo</td>
+          </tr>
+          <tr>
+            <td className={styles.td}>mkq12ie4</td>
+            <td className={styles.td}>Leo</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
